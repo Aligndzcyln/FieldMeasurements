749ffe264da833b5e079d7d349eaf8a7623f5738
Ali Gündüz <ceylanali80@gmail.com>
2019-05-30 16:27:35 +0300

    fixed bugs

--- Field measurement/Program.cs
+++ Field measurement/Program.cs
@@ -10,19 +10,19 @@ namespace Field_measurement
     {
         static void Main(string[] args)
         {
-            circle circle = new circle();
-            coordinate topLeft = new coordinate();
-            topLeft.coords(12, 20);
-            circle.c = topLeft;
-            circle.r = 5;
-            rectangle rectangle = new rectangle();
-            rectangle.c = topLeft;
-            rectangle.height = 30;
-            rectangle.height = 50;
-            Console.WriteLine("circle area: " + calculation.circle_area(circle.r));
-            Console.WriteLine("circle circumference: " + calculation.circle_circumference(circle.r));
-            Console.WriteLine("rectangle circumference: " + calculation.rectangle_circumference(rectangle.height,rectangle.width));
-            Console.WriteLine("rectangle area: " + calculation.rectangle_area(rectangle.height, rectangle.width));
+            circle Circle = new circle();
+            coordinate TopLeft = new coordinate();
+            TopLeft.coords(12, 20);
+            Circle.c = TopLeft;
+            Circle.r = 5;
+            rectangle Rectangle = new rectangle();
+            Rectangle.c = TopLeft;
+            Rectangle.height = 30;
+            Rectangle.width = 50;
+            Console.WriteLine("circle area: " + calculation.circle_area(Circle.r));
+            Console.WriteLine("circle circumference: " + calculation.circle_circumference(Circle.r));
+            Console.WriteLine("rectangle circumference: " + calculation.rectangle_circumference(Rectangle.height,Rectangle.width));
+            Console.WriteLine("rectangle area: " + calculation.rectangle_area(Rectangle.height, Rectangle.width));
 
 
         }
